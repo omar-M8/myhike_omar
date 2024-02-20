c9ca5318ef0c270e7f5eb6b233204c928ba1d7df
OMAR_MOHAMED <mohammed.omar078@gmail.com>
2024-02-20 13:04:07 -0800

    Update code with google wirefall

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDP_x7Hb4WF-0PYuW3YZZRkn2zEjdPeD8M",
+    authDomain: "comp1800-202410-demo-79f26.firebaseapp.com",
+    projectId: "comp1800-202410-demo-79f26",
+    storageBucket: "comp1800-202410-demo-79f26.appspot.com",
+    messagingSenderId: "691821495325",
+    appId: "1:691821495325:web:0b7bfd68b4fe4e89e772f4"
 };
 
 //--------------------------------------------
